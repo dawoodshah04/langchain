eacd64e747fe0b92350682e158ce8a97b3baa902
dawood <dawood0963@gmail.com>
2026-09-25 10:44:42 +0500

    chatModels and LLMs configured via langchain

---
+++ ChatModels/chatmodel_hf.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation",
+    huggingfacehub_api_token=os.getenv("HF_ACCESS_TOKEN"),
+)
+
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("What's the capital of the US?")
+
+print(result)
